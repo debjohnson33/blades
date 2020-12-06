716229959c9b7e5c96b14ea3ad4a376e0706935a
Deb Johnson <friendswithdrj@gmail.com>
2020-12-06 15:21:12 -0600

    Add state/componentDidMount/render of goats

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      goats: [],
+      currentGoat: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/goats')
+      .then(res => {
+        const goats = res.data;
+        this.setState({
+          goats: goats,
+          currentGoat: goats[0]
+        });
+      });
+  }
+
   render() {
+    const { goats } = this.state;
     return (
-      <h1>Goats listed here!</h1>
+      <div>
+        <h1>Goats List!</h1>
+        {goats.map(goat => <div><li>{goat.name}</li> <p>{goat.description}</p></div>)}
+      </div>
     )
   }
 }
