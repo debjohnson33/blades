83d7a0ff2cc8395f91fd18faa682e40505510357
Deb Johnson <friendswithdrj@gmail.com>
2021-07-16 15:26:43 -0500

    Edit colors/add CSS to prod webpack

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -17,6 +17,10 @@ module.exports = {
             presets: ['@babel/preset-env', "@babel/preset-react"]
           }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ['css-loader']
       }
     ]
   }
