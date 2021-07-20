d24a9fa048c99d7be230b9410495149ca02307bd
Deb Johnson <friendswithdrj@gmail.com>
2021-07-20 16:44:17 -0500

    Add edit blade button to details

--- client/src/components/BladeDetails.jsx
+++ client/src/components/BladeDetails.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../../../public/styles.css';
 
-const BladeDetails = ({blade}) => {
+const BladeDetails = ({blade, selectEditMode}) => {
+  const handleEditMode = () => {
+    selectEditMode(blade.id);
+  }
   return (
     <div className='bladeDetails'>
       <h2>Stens: {blade.stens}</h2>
       <p><b>Manufacturer:</b>  {blade.manufacturer}</p>
       <p><b>Quantity:</b> {blade.quantity}</p>
+      <button onClick={() => handleEditMode()}>Edit this Blade</button>
     </div>
   )
 }
